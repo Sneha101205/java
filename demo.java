3e586c58fb86e62ec100424c19a43bf68e9588a6
Sneha101205 <achusnehu10@gmail.com>
2025-03-18 21:19:49 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class demo {
+public static void main(String srgs[]) {
+System.out.println("hello");
+
+	   		
+
+	}
+
+	}
+
+
+
+
---
+++ userinput.java
@@ -0,0 +1,24 @@
+package day1;
+import java.util.*;
+public class userinput {
+	public static void main(String args[]) {
+
+	    Scanner S1=new Scanner(System.in);
+
+	   System.out.println("enter a name:");
+
+	   String name=S1.nextLine();
+
+	   System.out.println("My Name is"+name);
+
+	     System.out.println("enter a mark:");
+
+	     int marks=S1.nextInt();
+
+	     System.out.println("mark is"+marks);
+
+	}
+
+	}
+
+
