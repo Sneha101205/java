599c691867d6e3ac5bb8da5f9aff3bb466ea4ca7
Sneha101205 <achusnehu10@gmail.com>
2025-03-19 20:34:21 +0530

    Add files via upload

---
+++ Stringoperations.java
@@ -0,0 +1,14 @@
+package day2;
+import java.util.*;
+public class Stringoperations {
+	public static void main(String[] args)
+	{
+		//String name = "ram";
+		String name = new String("Ram");
+		System.out.println(name);
+	
+		
+
+	}
+
+}
---
+++ Wrapperclass.java
@@ -0,0 +1,18 @@
+package day2;
+import java.util.*;
+public class Wrapperclass {
+	public static void mai(String args[]) {
+ /*Integer myInt=Integer.valueOf(100);
+ Double myDouble=Double.valueOf(12.34);
+ 
+ Character myChar=Character.valueOf('A');
+ Boolean myBool=Boolean.valueOf(true);*/
+		
+//autoboxing
+		Integer intObj=50;
+		
+//unboxing		
+		int intValue=intObj;
+		System.out.println(intValue);
+}
+}
